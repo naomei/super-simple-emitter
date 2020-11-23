12f860ddcd2c05d26d48f4e6c6cbbb83553912a3
Naohiro Urayama <naohiro.urayama@gmail.com>
2020-11-23 22:09:40 +0900

    a

--- src/emitters.ts
+++ src/emitters.ts
@@ -24,6 +24,32 @@ export const simpleEmitter = <P = any>() => {
 /**
  * 
  */
-export const emitter = <P = any>() => {
+// export const emitter = <P = any>() => {
 
+// };
+
+type Action<T, P> = {type: T, payload?: P};
+
+const createAction = <P>(type: string): Action<string, P> => {
+  return {type};
 };
+
+type CreateAction = ReturnType<typeof createAction>;
+
+export const emitter = {  
+  on: <A extends CreateAction>(action: A, fn?: (payload: A["payload"]) => void) => {
+
+  },
+  emit: <A extends CreateAction>(action: A, payload: A["payload"]) => {
+
+  }
+}
+
+
+// const e = createEmitter();
+
+// const ac = createAction<number>("name");
+
+
+// e.on(ac, (p) => {})
+// e.emit(ac, 10)
